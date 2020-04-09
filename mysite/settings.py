f07af4555d898ed99d287ee667d8e9836ca99ca6
Anuj Patel <anujpatel360@gmail.com>
2020-04-09 09:48:57 +0530

    removed localhost

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jgds5an#nqogh8-*u*c3t(qtc!^d-!ki0#j-&l=x^q5xe6h(f!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['anuj.pythonanywhere.com']
 
 
 # Application definition
